c698a305f51afaf0dadd60920964c4b31d03565d
Mansour Koura <Mansourashraf173@gmail.com>
2021-10-24 16:17:38 +0200

    Done Day02

--- BASICS/03_13-Chall03-Temp-Conv.js
+++ BASICS/03_13-Chall03-Temp-Conv.js
@@ -1,8 +1,6 @@
 let fahrebheit = 32;
-
-let celsius = (fahrebheit - 32) * (5/9);
-
-let kelvin = (fahrebheit - 459.67) * (5/9);
+let celsius    = (fahrebheit - 32)     * (5/9);
+let kelvin     = (fahrebheit - 459.67) * (5/9);
 
 console.log(celsius)
 console.log(kelvin)
---
+++ Functions/04_21-Chall08-Temp-Fun.js
@@ -0,0 +1,20 @@
+let toCelsius = function (fahrebheit) {
+    let temp = (fahrebheit - 32)     * (5/9);
+    return temp;
+}
+let toKelvin = function (fahrebheit) {
+    let temp = (fahrebheit - 459.67) * (5/9);
+    return temp;
+}
+
+let Celsius = toCelsius(32)
+console.log(Celsius)
+
+Celsius = toCelsius(68)
+console.log(Celsius)
+
+let Kelvin = toKelvin(495.67)
+console.log(Kelvin)
+
+Kelvin = toKelvin(459.67)
+console.log(Kelvin)
