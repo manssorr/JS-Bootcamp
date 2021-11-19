b666a93b0842677c9d73eaac09b25735389c75bc
Mansour Koura <Mansourashraf173@gmail.com>
2021-11-19 07:31:42 +0200

    Done 126

--- todo-app/scripts/todo-app.js
+++ todo-app/scripts/todo-app.js
@@ -1,39 +1,37 @@
 'use strict'
 
 const todos = getSavedTodo()
-
 const filters = {
     searchText: "",
     hideCompleted: false
 }
-
 renderTasks(todos, filters)
 
 document.querySelector('#text-task').addEventListener('input', (event) => {
-    console.log(event.target.value)
     filters.searchText  = event.target.value
     renderTasks(todos, filters)
-
 })
 
 // Add new note button Call
-document.querySelector('#add-todo').addEventListener('submit', (event) => {
+document.querySelector('#new-todo').addEventListener('submit', (event) => {
+    const text = event.target.todoText.value.trim()
     event.preventDefault()
-    todos.push({
-        id: uuidv4(),
-        text: event.target.todoText.value,
-        completed: false
-    })
 
-    saveTodo(todos)
-    renderTasks(todos, filters)
-    event.target.todoText.value = ""
+    if(text.length > 0) {
+        todos.push({
+            id: uuidv4(),
+            text,
+            completed: false
+        })
+        saveTodo(todos)
+        renderTasks(todos, filters)
+        event.target.todoText.value = ""
+    }
 })
 
 // Hide Complete Fiilter Call
 document.querySelector('#hide-complete').addEventListener('change', (event) => {
     filters.hideCompleted = event.target.checked
-    console.log(filters.hideCompleted)
     renderTasks(todos, filters)
 })
 
--- todo-app/scripts/todo-functions.js
+++ todo-app/scripts/todo-functions.js
@@ -17,18 +17,25 @@ const saveTodo = (todos) => {
 
 // render the todo list
 const renderTasks = (todos, filters) => {
+    const todoEl = document.querySelector('#tasks') 
     let filteredTasks = todos.filter((todo) => todo.text.toLowerCase().includes(filters.searchText.toLowerCase()))
-
     filteredTasks = filteredTasks.filter((todo) => !filters.hideCompleted || !todo.completed)
-    
     const incompletedTodos = filteredTasks.filter((todo) => !todo.complete)
+    
+    todoEl.innerHTML = ""
+    todoEl.appendChild(generateSammaryDOM(incompletedTodos))
 
-    document.querySelector('#tasks').innerHTML = ""
-    document.querySelector('#tasks').appendChild(generateSammaryDOM(incompletedTodos))
-
-    filteredTasks.forEach((todo) => {
-        document.querySelector('#tasks').appendChild(generateTodoDOM(todo))
-    })
+    if(filteredTasks.length > 0){
+        filteredTasks.forEach((todo) => {
+            todoEl.appendChild(generateTodoDOM(todo))
+        })
+    } else {
+        const emptyMeaageEl = document.createElement('p')
+        emptyMeaageEl.classList.add('empty-message') 
+        emptyMeaageEl.textContent = 'No to-dos to show!'
+        todoEl.appendChild(emptyMeaageEl)
+    }
+    
 }
 
 // Remove some todos by (x) button
@@ -49,33 +56,39 @@ const toggleTodo = (id) => {
 
 // Create the DOM structure of todo
 const generateTodoDOM = (todo) => {
-    const todoEl    = document.createElement('div')
-    let checkEl     = document.createElement('input')
-    const textEl    = document.createElement('span')
-    const removeBTN = document.createElement('button')
+    const todoEl       = document.createElement('label')
+    const containerEl  = document.createElement('div')
+    const checkEl      = document.createElement('input')
+    const todoText     = document.createElement('span')
+    const removeButton = document.createElement('button')
 
 
 
-    // Checkbox setup
+    // Setup checkbox 
     checkEl.setAttribute('type', 'checkbox')
-    todoEl.appendChild(checkEl)
     checkEl.checked = todo.completed
-
+    containerEl.appendChild(checkEl)
     checkEl.addEventListener('change', () => {
         toggleTodo(todo.id)
         saveTodo(todos)
         renderTasks(todos, filters)
-
     })
 
-    // todo text setup
-    textEl.textContent = todo.text
-    todoEl.appendChild(textEl)
+    // Setup todo text
+    todoText.textContent = todo.text
+    containerEl.appendChild(todoText)
 
-    // remove button setup
-    removeBTN.textContent = 'x'
-    todoEl.appendChild(removeBTN)
-    removeBTN.addEventListener('click', () => {
+    // Setup container
+    todoEl.classList.add('list-item')
+    containerEl.classList.add('list-item__container')
+    todoEl.appendChild(containerEl)
+
+
+    // Setup remove button
+    removeButton.textContent = 'remove'
+    removeButton.classList.add('button', 'button--text')
+    todoEl.appendChild(removeButton)
+    removeButton.addEventListener('click', () => {
         removeTodo(todo.id)
         saveTodo(todos)
         renderTasks(todos, filters)
@@ -86,7 +99,9 @@ const generateTodoDOM = (todo) => {
 // Create Sammary DOM 
 const generateSammaryDOM = (incompletedTodos) => {
     const summary = document.createElement('h2')
-    summary.textContent = `You have ${incompletedTodos.length} tasks left`
+    summary.classList.add('list-title')
+    const numTodos = incompletedTodos.length === 1 ? '' : 's'
+    summary.textContent = `You have ${incompletedTodos.length} task${numTodos} left`
     return summary
 }
 
